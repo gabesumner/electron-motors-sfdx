9803f3b3eb32cd83c7e5c61d9a1e406b91d48e89
Gabe Sumner <gsumner@gsumner-ltm7.internal.salesforce.com>
2019-06-01 16:22:52 -0700

    Fixes for SFDX deployer

--- orgInit.sh
+++ orgInit.sh
@@ -2,17 +2,15 @@
 sfdx force:org:create -f config/project-scratch-def.json -s -w 60
 
 # Analytics doesn't deploy without a permset and that will block the deployment of the permset
-# Copy a special .forceignore file that will ignore all Analytics resources 
-cp .forceignore.pre .forceignore
+# The .forceignore file will exclude the problematic files.
 sfdx force:source:push
 sfdx force:user:permset:assign -n electron
 sfdx force:apex:execute -f scripts/assignAnalyticsPermset.apex
 
 # The Permset is now deployed and applied, so we can restore the original .forceignore
-# Then we can deploy all Analytics resources.
-cp .forceignore.post .forceignore
+# Now deploy the files we skipped.
 sfdx force:source:deploy --sourcepath ./force-app/main/default/wave
-sfdx force:source:deploy --sourcepath ./force-app/main/default/flexipages
+sfdx force:source:deploy --sourcepath ./force-app/main/default/flexipages/Finance_Home1.flexipage-meta.xml
 
 # Create the demo accounts
 sfdx force:apex:execute -f scripts/createDemoRecords.apex
